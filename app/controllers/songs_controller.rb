6ac810763e54657b8fff7553b98df3ac0941b3b2
Charlean Baxter <baxtercm@outlook.com>
2023-01-19 22:32:48 -0800

    create endpoint, added images of postman and rails routes, added more notes per lecture discussions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,4 +8,18 @@ class SongsController < ApplicationController
     song = Song.find(params[:id])
     render json: song
   end
+
+  def create
+    song = Song.create(song_params)
+    if song.valid?
+      render json: song
+    else
+      render json: song.errors
+    end
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:title, :artist)
+  end
 end
